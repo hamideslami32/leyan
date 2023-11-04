8722c6ba32609dbae96e0deff595b6718ec7033e
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2023-11-04 21:45:14 +0330

    feat: add theme configuration

---
+++ src/theme/colors.ts
@@ -0,0 +1,55 @@
+export const primary = {
+  100: '#E3EEFF',
+  200: '#E3EEFF',
+  300: '#71AAFF',
+  400: '#3987FF',
+  500: '#0065FF',
+  600: '#004FC6',
+  700: '#00388E',
+  800: '#002255',
+  900: '#000B1C',
+};
+
+export const secondary = {
+  100: '#F9FAFB',
+  200: '#F4F6F8',
+  300: '#DFE3E8',
+  400: '#C4CDD5',
+  500: '#919EAB',
+  600: '#637381',
+  700: '#454F5B',
+  800: '#212B36',
+  900: '#161C24',
+};
+
+export const error = {
+  100: '#FFD1D4',
+  200: '#FFACB2',
+  300: '#FF8B93',
+  400: '#FF737C',
+  500: '#FF5A65',
+};
+
+export const warning = {
+  100: '#FFF1C6',
+  200: '#FFE599',
+  300: '#FFDB70',
+  400: '#FFD454',
+  500: '#FFCC33',
+};
+
+export const success = {
+  100: '#B9EED5',
+  200: '#82E0B3',
+  300: '#50D495',
+  400: '#2DCB80',
+  500: '#05C168',
+};
+
+export const info = {
+  lighter: '#CAFDF5',
+  light: '#61F3F3',
+  DEFAULT: '#00B8D9',
+  dark: '#006C9C',
+  darker: '#003768',
+};
