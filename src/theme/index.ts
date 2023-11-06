e5614c63a1325b06299a3658164270cb5744d3d4
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2023-11-06 11:10:54 +0330

    feat: use input + textarea + button style configs on extendTheme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
 import * as colors from './colors';
 
+import { Input } from './input';
+import { Textarea } from './textarea';
+import { Button } from './button';
+
 export const theme = extendTheme({
   colors: colors,
+  components: {
+    Input,
+    Textarea,
+    Button,
+  },
 });
