3c022e2075aed73bace442d2d0f9b060cc2c4af3
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2023-11-10 13:53:06 +0330

    feat: add two variants to accordion

--- src/theme/accordion.ts
+++ src/theme/accordion.ts
@@ -4,16 +4,36 @@ import { accordionAnatomy } from '@chakra-ui/anatomy';
 const helper = createMultiStyleConfigHelpers(accordionAnatomy.keys);
 
 export const Accordion = helper.defineMultiStyleConfig({
-  baseStyle: {
-    container: {
-      border: 'none',
-      borderBottom: '1px #E6E8EC solid',
+  variants: {
+    faq: {
+      container: {
+        border: 'none',
+        borderBottom: '1px #E6E8EC solid',
+      },
+      button: {
+        py: 6,
+      },
+      panel: {
+        color: 'secondary.600',
+      },
     },
-    button: {
-      py: 6,
-    },
-    panel: {
-      color: 'secondary.600',
+    filter: {
+      container: {
+        border: 'none',
+      },
+      button: {
+        p: 0,
+        fontSize: 'lg',
+      },
+      icon: {
+        color: 'secondary.500',
+        w: '24px',
+        h: '24px',
+      },
+      panel: {
+        px: 0,
+        py: 2,
+      },
     },
   },
 });
