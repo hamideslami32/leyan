4f68b7b1531dcf1dcf83a8ed259d3b3d6bf36cf6
Hamid Eslami <heslami32@gmail.com>
2023-12-31 18:32:34 +0330

    feat:  thank you page success

--- src/pages/factor-result/index.tsx
+++ src/pages/factor-result/index.tsx
@@ -1,5 +1,25 @@
+import Image from 'next/image';
+
 const FactorResult = () => {
-  return <div>FactorResult</div>;
+  return (
+    <div className='mx-auto my-16 grid max-w-cs place-items-center'>
+      <Image
+        src={'/images/success-purchase.png'}
+        width={176}
+        height={176}
+        alt='factor-result'
+      />
+      <p className='my-10 text-2xl font-bold text-success'>
+        سفارش شما با موفقیت ثبت شد
+      </p>
+      <p className='text-center leading-8'>
+        از قسمت مدیریت سفارشات می‌توانید روند پردازش سفارشتان را پیگیری کنید.
+        <br />
+        همچنین در صورت داشتن هرگونه سوال مربوط به سفارشات در روزهای کاری از ساعت
+        ۹ الی ۱۸ با شماره تماس ۲۴۸۱۰۰-۰۲۱ تماس بگیرید
+      </p>
+    </div>
+  );
 };
 
 export default FactorResult;
